589321ec878281ab7bb487986eb934cd7d93ccf2
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-29 01:46:42 +0530

    Details added in files

--- src/test/java/awesomecucumber/domainobjects/BillingDetails.java
+++ src/test/java/awesomecucumber/domainobjects/BillingDetails.java
@@ -1,3 +1,15 @@
+/**
+ * @author Rajat Verma
+ * https://www.linkedin.com/in/rajat-v-3b0685128/
+ * https://github.com/rajatt95
+ * https://rajatt95.github.io/
+ *
+ * Course: Cucumber BDD Masterclass with Selenium 4 & Java + Framework (https://www.udemy.com/course/cucumber-bdd-masterclass/)
+ * Tutor: Omprakash Chavan (https://www.udemy.com/user/omprakash-chavan/)
+ */
+
+/***************************************************/
+
 package awesomecucumber.domainobjects;
 
 public class BillingDetails {
